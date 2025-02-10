31197ba42f5a05889c72266387bcf984ffc94a5a
kevinletexier <kevin.le.texier@gmail.com>
2025-02-10 08:00:47 +0100

    Add thymeleaf front test

--- src/main/java/fr/sawox/blog/adapter/in/web/CreatePostController.java
+++ src/main/java/fr/sawox/blog/adapter/in/web/CreatePostController.java
@@ -3,15 +3,22 @@ package fr.sawox.blog.adapter.in.web;
 import fr.sawox.blog.application.domain.model.Post;
 import fr.sawox.blog.application.port.in.CreateAPostUseCase;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @AllArgsConstructor
 public class CreatePostController {
 
     private final CreateAPostUseCase createAPostUseCase;
+
+    @GetMapping("/post")
+    String getFormPost() {
+        return "forms/formPost :: add-post";
+    }
 
     @PostMapping("/post")
     void createPost(@RequestBody Post.PostSnapshot postSnapshot) {
--- src/main/java/fr/sawox/blog/adapter/in/web/RetrievePostController.java
+++ src/main/java/fr/sawox/blog/adapter/in/web/RetrievePostController.java
@@ -3,19 +3,29 @@ package fr.sawox.blog.adapter.in.web;
 import fr.sawox.blog.application.domain.model.Post;
 import fr.sawox.blog.application.port.in.RetrieveAPostUseCase;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
+@Controller
 @AllArgsConstructor
+@RequestMapping("/")
 public class RetrievePostController {
 
     private final RetrieveAPostUseCase retrievePostUseCase;
 
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/posts")
-    public List<Post.PostSnapshot> getPosts() {
-        return retrievePostUseCase.retrieveAll();
+    public String getPosts(Model model) {
+        List<Post.PostSnapshot> posts = retrievePostUseCase.retrieveAll();
+        model.addAttribute("posts", posts);
+        return "fragments :: posts-list";
     }
 }
